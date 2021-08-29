82c4d4bbcf90512f22835bc55a24d370aca514bc
sonaligadge24 <sonaligadge24@gmail.com>
2021-08-29 07:55:29 +0530

    Initialization of code

---
+++ src/main/java/com/bridgelabz/SnakeAndLadder/SnakeLadder.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.SnakeAndLadder;
+
+
+public class SnakeLadder {
+	public static void main(String[] args) {
+		int position1 = 0;
+		System.out.println("Welcome to Snake and Ladder Game");
+		System.out.println("Position of Player1 is:" + position1);
+	}
+}
