b2171e277f2613d0fbb51ea1fef42e0985c82868
sonaligadge24 <sonaligadge24@gmail.com>
2021-08-29 09:57:16 +0530

    Updated code for use case 3

--- src/main/java/com/bridgelabz/SnakeAndLadder/SnakeLadder.java
+++ src/main/java/com/bridgelabz/SnakeAndLadder/SnakeLadder.java
@@ -1,11 +1,16 @@
 package com.bridgelabz.SnakeAndLadder;
 
-
 public class SnakeLadder {
+
 	private static int rollCheck() {
 		int rollValue = (int) (Math.floor(Math.random() * 10 % 7));
 		return rollValue;
 
+	}
+
+	private static int options() {
+		int option = (int) (Math.floor(Math.random() * 10 % 3));
+		return option;
 	}
 
 	public static void main(String[] args) {
@@ -13,9 +18,28 @@ public class SnakeLadder {
 		System.out.println("Welcome to Snake and Ladder Game");
 		System.out.println("Position of Player1 is:" + position1);
 		int rollDie = rollCheck();
-		if (rollDie > 0)
+		if (rollDie > 0) {
 			System.out.println("Value at die is: " + rollDie);
-		else
+			int checkOption = options();
+			switch (checkOption) {
+			case 0:
+				System.out.println("No play");
+				break;
+			case 1:
+				System.out.println("Ladder");
+				position1 = position1 + rollDie;
+				break;
+			case 2:
+				System.out.println("Snake");
+				position1 = position1 - rollDie;
+				if (position1 < 0)
+					position1 = 0;
+				break;
+			}
+			System.out.println("Current Position of player1 is: " + position1);
+
+		} else
 			System.out.println("Roll the die again!");
+
 	}
 }
